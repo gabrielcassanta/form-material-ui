75bd25f8998159b45f4ba980c9c384cc951a897a
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-08-11 12:07:56 -0300

    init commit

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
+
+function FormularioCadastro() {
+  return (
+    <form>
+      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth required/>
+      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth required/>
+      <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth required/>
+
+      <label>Promoções</label>
+      <input type="checkbox" />
+      <label>Novidades</label>
+      <input type="checkbox" />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default FormularioCadastro;
