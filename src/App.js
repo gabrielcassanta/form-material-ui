38135376a8dc674071d6ef121683da232692280e
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-08-13 10:33:47 -0300

    3 funcs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 
-import {Container, Typography } from "@material-ui/core"
+
+import { Container, Typography } from "@material-ui/core"
 class App extends Component {
   render() {
     return (
@@ -14,15 +15,15 @@ class App extends Component {
   }
 }
 
-function aoEnviarForm(dados){
+function aoEnviarForm(dados) {
   console.log(dados);
 }
 
-function validarCPF(cpf){
-  if(cpf.length !== 11){
-    return {valido:false, texto:"CPF deve ter 11 digitos."}
-  }else{
-    return {valido:true, texto:""}
+function validarCPF(cpf) {
+  if (cpf.length !== 11) {
+    return { valido: false, texto: "CPF deve ter 11 digitos." }
+  } else {
+    return { valido: true, texto: "" }
   }
 }
 
