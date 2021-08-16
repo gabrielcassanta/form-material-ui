9abe59a829de2f69ffbed807eb4a0eabf280720a
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-08-16 10:35:33 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Container, Typography } from "@material-ui/core";
 
+import ValidacoesCadastro from "./contexts/ValidacoesCadastro";
 
-import { Container, Typography } from "@material-ui/core"
+import { validarCPF, validarSenha } from "./models/cadastro";
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm">
-        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+        <Typography variant="h3" component="h1" align="center">
+          Formulário de cadastro
+        </Typography>
+        <ValidacoesCadastro.Provider
+          value={{ cpf: validarCPF, senha: validarSenha, nome: validarSenha }}
+        >
+          <FormularioCadastro aoEnviar={aoEnviarForm} />
+        </ValidacoesCadastro.Provider>
       </Container>
     );
   }
@@ -17,14 +25,6 @@ class App extends Component {
 
 function aoEnviarForm(dados) {
   console.log(dados);
-}
-
-function validarCPF(cpf) {
-  if (cpf.length !== 11) {
-    return { valido: false, texto: "CPF deve ter 11 digitos." }
-  } else {
-    return { valido: true, texto: "" }
-  }
 }
 
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -4,7 +4,7 @@ import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 import { Typography, Stepper, Step, StepLabel } from "@material-ui/core";
 
-function FormularioCadastro({ aoEnviar, validarCPF }) {
+function FormularioCadastro({ aoEnviar }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDados] = useState({});
 
@@ -12,13 +12,13 @@ function FormularioCadastro({ aoEnviar, validarCPF }) {
     if (etapaAtual === formularios.length - 1) {
       aoEnviar(dadosColetados);
     }
-  })
+  });
 
   const formularios = [
     <DadosUsuario aoEnviar={coletarDados} />,
-    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosPessoais aoEnviar={coletarDados} />,
     <DadosEntrega aoEnviar={coletarDados} />,
-    <Typography variant="h5">Obrigado pelo Cadastro!</Typography>
+    <Typography variant="h5">Obrigado pelo Cadastro!</Typography>,
   ];
 
   function coletarDados(dados) {
@@ -28,15 +28,25 @@ function FormularioCadastro({ aoEnviar, validarCPF }) {
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
-  return <>
-    <Stepper activeStep={etapaAtual}>
-      <Step><StepLabel>Login</StepLabel></Step>
-      <Step><StepLabel>Pessoal</StepLabel></Step>
-      <Step><StepLabel>Entrega</StepLabel></Step>
-      <Step><StepLabel>Finalização</StepLabel></Step>
-    </Stepper>
-    {formularios[etapaAtual]}
-  </>;
+  return (
+    <>
+      <Stepper activeStep={etapaAtual}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
+      </Stepper>
+      {formularios[etapaAtual]}
+    </>
+  );
 }
 
 export default FormularioCadastro;
